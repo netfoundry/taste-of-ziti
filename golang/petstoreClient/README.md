1e7d978f0308fe51ac41ea95f4db9703dd06dc04
Alan Hazelton <alan.hazelton@netfoundry.io>
2024-01-17 15:01:18 -0500

    Renamed to petstoreClient and split Aperitivo functionality to separate go file

--- golang/petstoreClient/README.md
+++ golang/petstoreClient/README.md
@@ -1,33 +1,24 @@
-# OpenZiti PetstoreGo
+# OpenZiti PetstoreClient
 
 This example is a simple golang client that connects to a Petstore server using OpenZiti. 
 
 ## Setup :wrench:
-This example uses the Taste-of-Ziti demo network and its aperitivo service to obtain a temporary identity. Refer to
+This example uses the Taste-of-Ziti demo network and its Aperitivo service to obtain a temporary identity. Refer to
 the [Taste-of-Ziti README](../../README.md) for additional details on the Taste-of-Ziti demo network.
 
 ## Requirements
 * go 1.21.x or later
 * gcc compiler
 
-## Build
-Execute the following to build and place the executable in a directory labeled `build`
-
-1. Run the following to create the build directory and build the project
-
-       mkdir build
-       go mod tidy
-       go build -o build ./...
-
 ## Usage
 
-By default, the petstoreGo client connects to a hosted OpenZiti network and receives a temporary identity.  The 
+By default, the petstoreClient connects to a hosted OpenZiti network and receives a temporary identity.  The 
 identity is saved and reused for subsequent calls.  An alternate identity file can be used by calling the client with
-the `-i <identityFile>` parameter or from the `ZITI_IDENTITIES` environment variable.
+the `-i <identityFile>` parameter.
 
 ### Example Output
 ```shell
-$ ./petstoreGo -h
+$ go run petstoreClient -h
   -a string
     	optional aperitivo url for acquiring an identity (default "https://aperitivo.production.netfoundry.io")
   -h	Display usage
@@ -36,15 +27,15 @@ $ ./petstoreGo -h
   -q string
     	petstore query to execute (default "/api/v3/pet/findByStatus?status=available")
 
-$ ./petstoreGo -q /api/v3/pet/findByStatus?status=sold
-INFO    Connecting to aperitivo at https://aperitivo.netfoundry.io to generate a new temporary identity 
+$ go run petstoreClient -q /api/v3/pet/findByStatus?status=sold
+INFO    Connecting to Aperitivo at https://aperitivo.production.netfoundry.io to generate a new temporary identity 
 INFO    generating 4096 bit RSA key                  
-INFO    A new identity is being enrolled and stored in taste_of_ziti.json. This is a temporary identity that is valid until Fri, 17 Nov 2023 18:48:11 UTC 
+INFO    A new identity is being enrolled and stored in /home/myDemoUser/ziti/taste_of_ziti.json. This is a temporary identity that is valid until Fri, 17 Nov 2023 18:48:11 UTC 
 INFO    Identity write completed                     
 INFO    Loading identity from taste_of_ziti.json     
 INFO    This identity provides access to the service: FireworksDemo 
 INFO    This identity provides access to the service: WhatIsMyIpDemo 
 INFO    This identity provides access to the service: PetstoreDemo 
-INFO    Dialing PetstoreDemo with query string from command line: /api/v3/pet/findByStatus?status=sold 
+INFO    Calling PetstoreDemo with query string from command line: /api/v3/pet/findByStatus?status=sold 
 INFO    Received: [{"id":5,"category":{"id":1,"name":"Dogs"},"name":"Dog 2","photoUrls":["url1","url2"],"tags":[{"id":1,"name":"tag2"},{"id":2,"name":"tag3"}],"status":"sold"}] 
 ```
---
+++ golang/petstoreClient/petstore.go
@@ -0,0 +1,173 @@
+/*
+	Copyright NetFoundry Inc.
+
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
+
+	https://www.apache.org/licenses/LICENSE-2.0
+
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
+*/
+
+package main
+
+import (
+  "context"
+  "flag"
+  "fmt"
+  "github.com/openziti/sdk-golang/ziti"
+  "io"
+  "log"
+  "net"
+  "net/http"
+  "os"
+)
+
+const defaultAperitivoUrl = "https://aperitivo.production.netfoundry.io"
+
+func parseOpts() map[string]string {
+  aperitivoPtr := flag.String("a", defaultAperitivoUrl, "optional Aperitivo url for acquiring an identity")
+  identityPtr := flag.String("i", "", "optional identity file")
+  queryPtr := flag.String("q", "/api/v3/pet/findByStatus?status=available", "petstore query to execute")
+  var showHelp bool
+  flag.BoolVar(&showHelp, "h", false, "Display usage")
+  flag.Parse()
+  if showHelp {
+    flag.PrintDefaults()
+    os.Exit(0)
+  }
+  parsedOpts := make(map[string]string)
+  if *identityPtr != "" {
+    parsedOpts["identity"] = *identityPtr
+  }
+  // Options with default values so just add
+  parsedOpts["query"] = *queryPtr
+  parsedOpts["aperitivo"] = *aperitivoPtr
+  return parsedOpts
+}
+
+// Reads the identity JSON file and adds the parsed ziti.Context into a ziti.CtxCollection
+func loadIdentity(identityFile string) *ziti.CtxCollection {
+  log.Printf("Loading identity from %s", identityFile)
+  ctx, err := ziti.NewContextFromFile(identityFile)
+  if err != nil {
+    log.Fatalf("Unable to load context from %s", identityFile)
+  }
+  collection := ziti.NewSdkCollection()
+  collection.Add(ctx)
+
+  return collection
+}
+
+// load or generate and then load the ziti Identity
+func checkCreateIdentity(opts map[string]string) *ziti.CtxCollection {
+  // First check to see if an identity file was specified on the command line.  If so, use it
+  if cmdLineIdentity, present := opts["identity"]; present {
+    return loadIdentity(cmdLineIdentity)
+  }
+  // Next check to see if the default identity file is available. If not, generate an identity into it
+  if _, err := os.Stat(DefaultIdentityFile); err != nil {
+    GenerateIdentity(opts["aperitivo"])
+  }
+  // Lastly, load the identity from the generated or pre-existing default identity file
+  return loadIdentity(DefaultIdentityFile)
+}
+
+func buildZitiClient(collection *ziti.CtxCollection) *http.Client {
+  zitiTransport := http.DefaultTransport.(*http.Transport).Clone() // copy default transport
+  zitiTransport.DialContext = func(_ context.Context, _, addr string) (net.Conn, error) {
+    // Need to search for a context within the collection that provides access to the service name in the address parameter
+    service, _, err := net.SplitHostPort(addr)
+    if err != nil {
+      return nil, err
+    }
+
+    var ztx ziti.Context
+    var matchFound = false
+    collection.ForAll(func(ctx ziti.Context) {
+      if matchFound {
+        return
+      }
+
+      if _, found := ctx.GetService(service); found {
+        ztx = ctx
+        matchFound = true
+      }
+    })
+
+    if matchFound {
+      return ztx.Dial(service)
+    }
+    return nil, fmt.Errorf("service [%s] is not available by any ziti context", service)
+  }
+  zitiTransport.TLSClientConfig.InsecureSkipVerify = true
+  return &http.Client{Transport: zitiTransport}
+}
+
+func checkIdentityIsStillValid(ctxCollection *ziti.CtxCollection) bool {
+  isValid := false
+  // iterate over the ziti.Context's in the collection and attempt to authenticate the Identity and then verify that the PetstoreDemo
+  // service can be accessed using this identity.
+  ctxCollection.ForAll(func(ctxItem ziti.Context) {
+    err := ctxItem.Authenticate()
+    if err != nil {
+      log.Printf("ERROR: Cannot authenticate with the configured identity. If using '%s', try deleting this saved identity file and trying again",
+        DefaultIdentityFile)
+      log.Fatal(err)
+    }
+    availableServices, err := ctxItem.GetServices()
+    if err != nil {
+      log.Fatal(err)
+    }
+    for _, svc := range availableServices {
+      log.Printf("This identity provides access to the service: %s", *svc.Name)
+      if *svc.Name == "PetstoreDemo" {
+        isValid = true
+        break
+      }
+    }
+  })
+  return isValid
+}
+
+func connectZitiService(ctxCollection *ziti.CtxCollection, opts map[string]string) {
+  if !checkIdentityIsStillValid(ctxCollection) {
+    log.Fatal("PetstoreDemo service not accessible by this Identity")
+  }
+  log.Printf("Calling PetstoreDemo with query string from command line: %s", opts["query"])
+  response, err := buildZitiClient(ctxCollection).Get("http://PetstoreDemo" + opts["query"])
+  if err != nil {
+    log.Fatal(err)
+  }
+  defer func(Body io.ReadCloser) {
+    err := Body.Close()
+    if err != nil {
+      log.Fatal(err)
+    }
+  }(response.Body)
+  responseData, err := io.ReadAll(response.Body)
+  if err != nil {
+    log.Fatal(err)
+  }
+  if response.StatusCode != 200 {
+    log.Printf("ERROR: Non-success response (%d) received from the petstore service on the query for %s.  Body received is: %s",
+      response.StatusCode, opts["query"], responseData)
+    return
+  }
+
+  log.Printf("Received: %s", responseData)
+
+}
+
+func main() {
+  userOpts := parseOpts()
+
+  zitiCtxCollection := checkCreateIdentity(userOpts)
+
+  connectZitiService(zitiCtxCollection, userOpts)
+}
